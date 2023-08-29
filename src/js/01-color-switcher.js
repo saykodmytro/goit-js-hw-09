0e1c11ee2ada12f74509072ffa73d2024d5063d4
Dmytro Sayko <saykodmytro@icloud.com>
2023-08-29 10:37:34 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const btnSatrt = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+btnSatrt.addEventListener('click', onBtnSatrtClick);
+btnStop.addEventListener('click', onBtnStopClick);
+let timerId = null;
+
+function onBtnSatrtClick() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnSatrt.disabled = true;
+}
+
+function onBtnStopClick() {
+  clearInterval(timerId);
+  btnSatrt.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
