b3cd20b973d15309927579f93df6955edd78e78b
saykodmytro <64913341+saykodmytro@users.noreply.github.com>
2023-08-29 07:38:00 +0000

    Deploying to gh-pages from @ saykodmytro/goit-js-hw-09@0e1c11ee2ada12f74509072ffa73d2024d5063d4 🚀

---
+++ 01-color-switcher.274296a2.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}));var n=null}();
+//# sourceMappingURL=01-color-switcher.274296a2.js.map
---
+++ 01-color-switcher.2e283c03.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1}));let a=null;
+//# sourceMappingURL=01-color-switcher.2e283c03.js.map
