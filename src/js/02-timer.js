d394896a3c96273ecf37778b101d05ee5833de03
Dmytro Sayko <saykodmytro@icloud.com>
2023-08-31 14:04:30 +0300

    up

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -8,15 +8,65 @@ Notiflix.Notify.init({
   position: 'center-top',
 });
 
+const inputEl = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const bodyEl = document.body;
+
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+startBtn.addEventListener('click', startTimer);
+startBtn.disabled = true;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates[0] <= options.defaultDate) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      startBtn.disabled = true;
+    }
+    if (selectedDates[0] >= options.defaultDate) {
+      startBtn.disabled = false;
+    }
   },
 };
+
+const library = flatpickr(inputEl, options);
+let timerId = null;
+
+function startTimer() {
+  timerId = setInterval(() => {
+    updateTimer();
+
+    inputEl.disabled = true;
+    inputEl.classList.add('active-timer');
+    startBtn.disabled = true;
+    startBtn.style.color = 'grey';
+  }, 1000);
+}
+
+function updateTimer() {
+  const currentTime = new Date();
+  const setTime = new Date(inputEl.value);
+  const deltaTime = setTime - currentTime;
+  const { days, hours, minutes, seconds } = convertMs(deltaTime);
+
+  daysEl.textContent = addLeadingZero(`${days}`);
+  hoursEl.textContent = addLeadingZero(`${hours}`);
+  minutesEl.textContent = addLeadingZero(`${minutes}`);
+  secondsEl.textContent = addLeadingZero(`${seconds}`);
+
+  if (deltaTime < 1000) {
+    clearInterval(timerId);
+    bodyEl.style.backgroundColor = 'red';
+    Notiflix.Notify.success('timer stoped');
+  }
+}
 
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
